85d46bcbab05dec5282a34cc2debdbbc014e7c38
maltesh1234 <109894465+malteshmech35@users.noreply.github.com>
2022-09-26 00:00:40 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.127.92.255:9000/',
+  serverUrl: 'http://15.206.147.209:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
